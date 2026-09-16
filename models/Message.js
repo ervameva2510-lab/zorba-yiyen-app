fdb3e7c33cda3555ba5d02801e6ed081d0b4d7dd
ervameva2510-lab <erva.meva2510@gmail.com>
2026-09-16 17:35:06 +0300

    Database - Message Model Schema

---
+++ models/Message.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  recipientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  },
+  isRead: {
+    type: Boolean,
+    default: false
+  },
+  messageType: {
+    type: String,
+    enum: ['text', 'invitation'],
+    default: 'text'
+  }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
